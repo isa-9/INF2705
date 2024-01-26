219806edb8e3ed69c20d05c3a8f43416d6361932
IsabellaSantacruz <isabella.santacruz@polymtl.ca>
2024-01-26 10:03:12 -0500

    Shapes done (?)

--- tp1_H24/src/shapes.cpp
+++ tp1_H24/src/shapes.cpp
@@ -24,6 +24,9 @@ BasicShapeArrays::~BasicShapeArrays()
     // TODO Partie 1: Supprimer la mémoire de l'objet.
     // Assurez-vous que les ressources ne soient pas utilisées
     // pendant la suppression.
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     glDeleteVertexArrays(1, &m_vao);
     glDeleteBuffers(1, &m_vbo);
 }
@@ -48,11 +51,35 @@ void BasicShapeArrays::draw(GLenum mode, GLsizei count)
 
 BasicShapeMultipleArrays::BasicShapeMultipleArrays(const GLfloat* pos, GLsizeiptr posByteSize, const GLfloat* color, GLsizeiptr colorByteSize)
 {
+    GLuint locPosition = 0;
+    GLint sizePosition = 3;
+    GLuint locColor = 1;
+    GLint sizeColor = 4;
+
     // TODO Partie 1: Générer et bind le vao de la forme.
+    glGenVertexArrays(1, &m_vao);
+    glBindVertexArray(m_vao);
     
     // TODO Partie 1: Générer et bind les vbos (un pour la position,
     // l'autre pour la couleur) de la forme. Allouer l'espace
     // nécessaire dans le mode approprié et envoyer les données au gpu.
+
+    // Position
+    glGenBuffers(1, &m_posVbo);
+    glBindBuffer(GL_ARRAY_BUFFER, m_posVbo);
+    glBufferData(GL_ARRAY_BUFFER, posByteSize, pos, GL_STATIC_DRAW);
+
+    enablePosAttribute(locPosition, sizePosition, 0, 0);
+
+    // Color
+    glGenBuffers(1, &m_colorVbo);
+    glBindBuffer(GL_ARRAY_BUFFER, m_colorVbo);
+    glBufferData(GL_ARRAY_BUFFER, colorByteSize, color, GL_STATIC_DRAW);
+
+    enableColorAttribute(locColor, sizeColor, 0, 0);
+
+    glBindVertexArray(0);
+
 }
 
 BasicShapeMultipleArrays::~BasicShapeMultipleArrays()
@@ -60,36 +87,55 @@ BasicShapeMultipleArrays::~BasicShapeMultipleArrays()
     // TODO Partie 1: Supprimer la mémoire de l'objet.
     // Assurez-vous que les ressources ne soient pas utilisées
     // pendant la suppression.
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glDeleteVertexArrays(1, &m_vao);
+    glDeleteBuffers(1, &m_posVbo);
+    glDeleteBuffers(1, &m_colorVbo);
 }
 
 void BasicShapeMultipleArrays::enablePosAttribute(GLuint index, GLint size, GLsizei stride, GLsizeiptr offset)
 {
     // TODO Partie 1: Activer l'attribut de position et l'attacher correctement au state du vao.
+    glVertexAttribPointer(index, size, GL_FLOAT, GL_FALSE, stride, &offset);
+    glEnableVertexAttribArray(index);
 }
 
 void BasicShapeMultipleArrays::enableColorAttribute(GLuint index, GLint size, GLsizei stride, GLsizeiptr offset)
 {
     // TODO Partie 1: Activer l'attribut de couleur et l'attacher correctement au state du vao.
+    glVertexAttribPointer(index, size, GL_FLOAT, GL_FALSE, stride, &offset);
+    glEnableVertexAttribArray(index);
 }
 
 void BasicShapeMultipleArrays::updateColorData(const GLfloat* color, GLsizeiptr colorByteSize)
 {
     // TODO Partie 1: Modifier la totalité des données de couleur
+    glBindBuffer(GL_ARRAY_BUFFER, m_colorVbo);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, colorByteSize, color);
 }
 
 GLfloat* BasicShapeMultipleArrays::mapPosData()
 {
     // TODO Partie 1: Activer le mapping des données de position
+    glBindBuffer(GL_ARRAY_BUFFER, m_posVbo);
+    glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
 }
 
 void BasicShapeMultipleArrays::unmapPosData()
 {
     // TODO Partie 1: Désactiver le mapping des données de position
+    glBindBuffer(GL_ARRAY_BUFFER, m_posVbo);
+    glUnmapBuffer(GL_ARRAY_BUFFER);
 }
 
 void BasicShapeMultipleArrays::draw(GLenum mode, GLsizei count)
 {
     // TODO Partie 1: Dessiner la forme.
+    glBindVertexArray(m_vao);
+    glDrawArrays(mode, 0, count);
+    glBindVertexArray(0);
 }
 
 
@@ -131,6 +177,9 @@ BasicShapeElements::~BasicShapeElements()
     // TODO Partie 1: Supprimer la mémoire de l'objet.
     // Assurez-vous que les ressources ne soient pas utilisées
     // pendant la suppression.
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     glDeleteVertexArrays(1, &m_vao);
     glDeleteBuffers(1, &m_vbo);
     glDeleteBuffers(1, &m_ebo);
