7bd5cda20f9dac4e04fa6f39be9ebf9a25a226bd
IsabellaSantacruz <isabella.santacruz@polymtl.ca>
2024-02-27 13:04:58 -0500

    TP2 - Water

--- tp1-2/src/shaders/water.fs.glsl
+++ tp1-2/src/shaders/water.fs.glsl
@@ -3,11 +3,16 @@
 in vec2 fragTexCoords;
 
 uniform sampler2D tex0;
+uniform float time;
 
 out vec4 outColor;
 
 void main() {
-	outColor = texture(tex0, fragTexCoords);
+	// Calcul des nouvelles coordonnées de texture
+	float offsetX = cos(time * 1.0 + (fragTexCoords.x + fragTexCoords.y) * 2.0) * 0.025f + time / 10;
+    float offsetY = sin(time * 2.0 + (fragTexCoords.x + fragTexCoords.y) * 2.0) * 0.01f;
+
+	outColor = texture(tex0, fragTexCoords + vec2(offsetX, offsetY));
 
 	if (outColor.a < 0.3)
 		discard;
--- tp1-2/src/shaders/water.vs.glsl
+++ tp1-2/src/shaders/water.vs.glsl
@@ -4,7 +4,6 @@ layout(location = 0) in vec3 position;
 layout(location = 1) in vec2 texCoords;
 
 uniform mat4 mvp;
-uniform float time;
 
 out vec2 fragTexCoords;
 
@@ -12,12 +11,5 @@ void main() {
 	vec4 transformed_position = mvp * vec4(position, 1.0);
 	gl_Position = transformed_position;
 
-	//fragTexCoords.x = 0.025f * cos(time / 10 * 1 + (texCoords.x + texCoords.y) * 2);
-	//fragTexCoords.y = 0.01f * sin(time / 10 * 3 + (texCoords.x + texCoords.y) * 2);
-
-	float offsetX = cos(time * 1.0 + (texCoords.x + texCoords.y) * 2.0) * 0.025f;
-    float offsetY = sin(time * 2.0 + (texCoords.x + texCoords.y) * 2.0) * 0.01f;
-
-    // Calcul des nouvelles coordonnées de texture
-    fragTexCoords = texCoords + vec2(offsetX, offsetY);
+    fragTexCoords = texCoords;
 }
